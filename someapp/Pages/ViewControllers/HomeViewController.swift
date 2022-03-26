ea5ee6e349249b5b533aea6fe275cd4f7c849938
yrehsef <yohai.reshef@gmail.com>
2022-03-26 12:25:18 +0300

    HomeVC

---
+++ someapp/Pages/ViewControllers/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  someapp
+//
+//  Created by Yohai(private) on 26/03/2022.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
